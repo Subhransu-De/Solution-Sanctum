4b26e97145a8f035f0f1a8d6e4be7d5876b8cf20
Subhransu De <subhransu.de.official@gmail.com>
2025-01-03 22:31:36 +0530

    Add Roman to Integer solution and corresponding tests

--- leetcode/test/test_Solution.py
+++ leetcode/test/test_Solution.py
@@ -1,5 +1,8 @@
-from leetcode.top_k_frequent_elements.Solution import Solution
 import pytest
+from leetcode.top_k_frequent_elements.Solution import (
+    Solution as solution_top_k_frequent,
+)
+from leetcode.roman_to_integer.Solution import Solution as solution_roman_to_integer
 
 
 @pytest.mark.parametrize(
@@ -15,4 +18,25 @@ def test_top_k_frequent(test_input):
     k = test_input["k"]
     expected = test_input["expected"]
 
-    assert Solution().topKFrequent(nums, k) == expected
+    assert solution_top_k_frequent().topKFrequent(nums, k) == expected
+
+
+@pytest.mark.parametrize(
+    "test_input",
+    [
+        {"s": "III", "expected": 3},
+        {"s": "LVIII", "expected": 58},
+        {"s": "MCMXCIV", "expected": 1994},
+        {"s": "IV", "expected": 4},
+        {"s": "IX", "expected": 9},
+        {"s": "XL", "expected": 40},
+        {"s": "XC", "expected": 90},
+        {"s": "CD", "expected": 400},
+        {"s": "CM", "expected": 900},
+    ],
+)
+def test_roman_to_int(test_input):
+    s = test_input["s"]
+    expected = test_input["expected"]
+
+    assert solution_roman_to_integer().romanToInt(s) == expected
